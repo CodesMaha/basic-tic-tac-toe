286681f3f6a9554975c82fa6b2d7faf6cd315132
Rashida Ijaz butt <mahaijazbutt@gmail.com>
2026-06-24 10:57:17 +0300

    hotfix: removal of line lead to `TypeError` because `core.mechanics.other_turn` returned `None` but `grid.Grid.modify_grid` did not account for `None`

--- core/grid.py
+++ core/grid.py
@@ -10,9 +10,6 @@ class Grid:
     def reset_grid(self) -> list[list[list[str]]]:
         self.grid = [['', '', ''] for _ in range(G_SIZE)]
 
-    def is_full(self) -> bool:
-        return all([[bool[b] for b in a] for a in self.grid])
-
     def is_slot_available(self, index: tuple[int, int]) -> bool:
         """ 
         check if grid slot is empty or not.
@@ -20,10 +17,11 @@ class Grid:
         """
         return not bool(self.grid[index[0]][index[1]])
 
-    def modify_grid(self, content: str, index: tuple[int, int]) -> None:
+    def modify_grid(self, content: str, index: tuple[int, int] | None) -> None:
         """ 
         replace grid index only if the spot has not been taken.
         truthy content intended. falsy index returns no change
-        TODO: bool for if changed
-        """
+        """ 
+        if not index:
+            return # for if index from core.mechanics.other_turn is None
         self.grid[index[0]][index[1]] = self.grid[index[0]][index[1]] or content
